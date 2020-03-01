dffff99e190e7cd308af2c44cf3f84221d2e5e81
Benjamin Wright <benjamin@famousstick.com>
2020-03-01 13:44:38 -0500

    added && before theme watch

--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,7 @@ module.exports = {
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       path: "../../dist/assets",
-      filename: "style.css"
+      filename: "theme.css"
     }),
     new BrowserSyncPlugin(
       // BrowserSync options
